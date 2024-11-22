05cdb551eb15522aedae6cad3f1e3744b36207d7
KazuyaSuzuki <83203852+ve1997@users.noreply.github.com>
2024-11-22 19:58:52 +0900

    Development workflow

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 import { createRequestHandler } from "@remix-run/express";
 import express from "express";
 
-import * as build from "./build/server/index.js";
+const viteDevServer =
+  process.env.NODE_ENV === "production"
+    ? null
+    : await import("vite").then((vite) =>
+        vite.createServer({
+          server: { middlewareMode: true },
+        })
+      );
 
 const app = express();
-app.use(express.static("build/client"));
+app.use(
+  viteDevServer ? viteDevServer.middlewares : express.static("build/client")
+);
+
+const build = viteDevServer
+  ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
+  : await import("./build/server/index.js");
 
 app.all("*", createRequestHandler({ build }));
 
